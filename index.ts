951d1c19c9071453312e505165284afa284a0729
Mikey Ying <mikeyinguk@gmail.com>
2023-09-09 10:44:00 +0100

    add book command

--- index.ts
+++ index.ts
@@ -1,2 +1,32 @@
 import { PrismaClient } from '@prisma/client'
+import { Command } from 'commander'
+
 const prisma = new PrismaClient()
+
+const program = new Command()
+
+program
+  .command('add <title> <author> <genre> <price> <quantity>')
+  .description('Add a book to the inventory')
+  .action(async (title, author, genre, price, quantity) => {
+    try {
+      const book = await prisma.book.create({
+        data: {
+          title,
+          author,
+          genre,
+          price: parseFloat(price),
+          quantity: parseInt(quantity),
+        },
+      });
+
+      console.log(`Added book: ${book.title}`);
+    } catch (error) {
+      console.error('Error adding book:', error);
+    } finally {
+      await prisma.$disconnect();
+    }
+  });
+
+  await program.parseAsync(process.argv);
+  
