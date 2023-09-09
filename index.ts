d739f56f90165f075d5b2699001bdab8371dc387
Mikey Ying <mikeyinguk@gmail.com>
2023-09-09 10:54:32 +0100

    command to get book by id

--- index.ts
+++ index.ts
@@ -61,4 +61,32 @@ program
     }
   })
 
+program
+  .command('get <id>')
+  .description('Get details of a book by its ID')
+  .action(async (id) => {
+    try {
+      const book = await prisma.book.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      })
+
+      if (book) {
+        console.log('Book Details:')
+        console.log(`Title: ${book.title}`)
+        console.log(`Author: ${book.author}`)
+        console.log(`Genre: ${book.genre}`)
+        console.log(`Price: $${book.price}`)
+        console.log(`Quantity: ${book.quantity}`)
+      } else {
+        console.log(`Book with ID ${id} not found.`)
+      }
+    } catch (error) {
+      console.error('Error getting book:', error)
+    } finally {
+      await prisma.$disconnect()
+    }
+  })
+
 await program.parseAsync(process.argv)
