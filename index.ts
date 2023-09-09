4db26228b707baf26a3949f2f8de5c0ab8430bb5
Mikey Ying <mikeyinguk@gmail.com>
2023-09-09 10:58:44 +0100

    delete by id command

--- index.ts
+++ index.ts
@@ -89,4 +89,33 @@ program
     }
   })
 
+program
+  .command('delete <id>')
+  .description('Delete a book by its ID')
+  .action(async (id) => {
+    try {
+      const book = await prisma.book.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      })
+
+      if (book) {
+        await prisma.book.delete({
+          where: {
+            id: parseInt(id),
+          },
+        })
+
+        console.log(`Deleted book with ID ${id}: ${book.title}`)
+      } else {
+        console.log(`Book with ID ${id} not found.`)
+      }
+    } catch (error) {
+      console.error('Error deleting book:', error)
+    } finally {
+      await prisma.$disconnect()
+    }
+  })
+
 await program.parseAsync(process.argv)
