9aab898d47b2cf1f11ec0e23c9ff1d34e2d95507
Mikey Ying <mikeyinguk@gmail.com>
2023-09-09 17:35:43 +0100

    move logic around for get by id

--- src/commands/get.ts
+++ src/commands/get.ts
@@ -1,39 +1,46 @@
 import { Command } from 'commander'
-import { PrismaClient } from '@prisma/client'
+import { getBookById } from '../db/services/bookService'
 import printer from '../utilities/printer'
+import { BookNotFoundError } from '../errors/customErrors'
+
+function handleGetBookError(error: unknown) {
+  if (error instanceof BookNotFoundError) {
+    printer.error(error.name, error.message)
+  } else {
+    console.error('Error adding book:', error)
+  }
+}
+
+async function handleGetBook(id: string) {
+  const parsedId = parseInt(id)
+
+  if (isNaN(parsedId)) {
+    console.error('Invalid ID. Please provide a valid number.')
+    return
+  }
+
+  try {
+    const book = await getBookById(parsedId)
+    if (book) {
+      const bookDetails = [
+        `Title: ${book.title}`,
+        `Author: ${book.author}`,
+        `Genre: ${book.genre}`,
+        `Price: $${book.price}`,
+        `Quantity: ${book.quantity}`,
+      ]
+
+      printer.log('Book Details:', ['magenta', 'inverse'])
+      printer.log(bookDetails.join('\n'))
+    }
+  } catch (error) {
+    handleGetBookError(error)
+  }
+}
 
 export function setupGetCommand(program: Command) {
   program
     .command('get <id>')
     .description('Get details of a book by its ID')
-    .action(async (id) => {
-      const prisma = new PrismaClient()
-
-      try {
-        const book = await prisma.book.findUnique({
-          where: {
-            id: parseInt(id),
-          },
-        })
-
-        if (book) {
-          const bookDetails = [
-            `Title: ${book.title}`,
-            `Author: ${book.author}`,
-            `Genre: ${book.genre}`,
-            `Price: $${book.price}`,
-            `Quantity: ${book.quantity}`,
-          ]
-
-          printer.log('Book Details:', ['magenta', 'inverse'])
-          printer.log(bookDetails.join('\n'))
-        } else {
-          printer.log(`Book with ID ${id} not found.`, ['red'])
-        }
-      } catch (error) {
-        console.error('Error getting book:', error)
-      } finally {
-        await prisma.$disconnect()
-      }
-    })
+    .action(handleGetBook)
 }
--- src/db/services/bookService.ts
+++ src/db/services/bookService.ts
@@ -1,5 +1,9 @@
 import { PrismaClient, Prisma } from '@prisma/client'
-import { DuplicateBookError, AddBookError } from '../../errors/customErrors'
+import {
+  DuplicateBookError,
+  AddBookError,
+  BookNotFoundError,
+} from '../../errors/customErrors'
 
 export async function createBook(
   title: string,
@@ -30,6 +34,32 @@ export async function createBook(
       throw new DuplicateBookError()
     } else {
       throw new AddBookError()
+    }
+  } finally {
+    await prisma.$disconnect()
+  }
+}
+
+export async function getBookById(id: number) {
+  const prisma = new PrismaClient()
+
+  try {
+    const book = await prisma.book.findUnique({
+      where: {
+        id: id,
+      },
+    })
+
+    if (book) {
+      return book
+    }
+
+    throw new BookNotFoundError(id)
+  } catch (error) {
+    if (!(error instanceof BookNotFoundError)) {
+      console.error('Error getting book:', error)
+    } else {
+      throw error
     }
   } finally {
     await prisma.$disconnect()
